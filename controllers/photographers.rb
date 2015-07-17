2f21dff70c30aae922225f3988ebb236c003ce9d
David Gardetti <dgardetti@gmail.com>
2015-07-16 22:22:55 -0500

    crud for photog (minus delete) done

--- controllers/photographers.rb
+++ controllers/photographers.rb
@@ -35,10 +35,14 @@ end
 get "/photographer/edited" do
   @photog_to_change = Photographer.find(params["photographer_id"].to_i)
   binding.pry
-  # if params["name"]@photog_to_change.name = params["name"]
-  @photog_to_change.age = params["age"]
+  if !params["name"].empty?
+    @photog_to_change.name = params["name"]
+  end
+  if !params["age"].empty?
+    @photog_to_change.age = params["age"]
+  end
   if @photog_to_change.valid?
-    # @photog_to_change.save
+    @photog_to_change.save
     "Photographer Changed"
     # for when I convert to json
     # @new_collab_hash = @new_collab.make_hash
--- models/album.rb
+++ models/album.rb
@@ -1,4 +1,5 @@
 
 class Album < ActiveRecord::Base
   has_and_belongs_to_many :photos
+  validates :title, presence: true
 end
