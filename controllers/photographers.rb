feafa45ce5f0fac7d30b8f102613db6ad3de2620
David Gardetti <dgardetti@gmail.com>
2015-07-16 21:24:47 -0500

    working on 'edit photog'

--- controllers/photographers.rb
+++ controllers/photographers.rb
@@ -1,9 +1,5 @@
 
 
-get "/" do
-  erb :"/home"
-end
-
 get "/photographers" do
   erb :"/photographers/main"
 end
@@ -14,15 +10,34 @@ get "/photographers/view_all" do
 end
 
 get "/photographers/add" do
-  @photographers = Photographer.all
   erb :"/photographers/add"
 end
 
-get "/photographer_added" do
+get "/photographer/added" do
   @new_photog = Photographer.new({"name" => params["name"], "age" => params["age"]})
   if @new_photog.valid?
     @new_photog.save
     "Photographer Added"
+    # for when I convert to json
+    # @new_collab_hash = @new_collab.make_hash
+    # json @new_collab_hash
+  else
+    @error = true
+    erb :"/photographers/add"
+  end
+end
+
+get "/photographers/edit" do
+  @photographers = Photographer.all
+  erb :"/photographers/edit"
+end
+
+get "/photographer/edited" do
+  @photog_to_change = Photographer.find(params["photographer_id"].to_i)
+  binding.pry
+  if @photog_to_change.valid?
+    # @photog_to_change.update
+    "Photographer Changed"
     # for when I convert to json
     # @new_collab_hash = @new_collab.make_hash
     # json @new_collab_hash
