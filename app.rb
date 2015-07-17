bb8afbcebade8172b1ecc8f2093036c7fa49109a
David Gardetti <dgardetti@gmail.com>
2015-07-17 10:54:49 -0500

    added production Heroku line to app

--- app.rb
+++ app.rb
@@ -9,6 +9,19 @@ configure :development do
   ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'photo_manager.db')
 end
 
+configure :production do  
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
 # So that ActiveRecord explains the SQL it's running in the logs.
 ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDOUT)
 
